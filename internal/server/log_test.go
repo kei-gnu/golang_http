6cf7d29a424fa06f28888ab3d4749ef399e58540
kei-gnu <61653118+kei-gnu@users.noreply.github.com>
2023-05-04 19:41:08 +0900

    update: test

--- internal/server/log_test.go
+++ internal/server/log_test.go
@@ -3,13 +3,14 @@ package server
 import (
 	"reflect"
 	"testing"
+	"fmt"
 )
 
 func TestLogAppend(t *testing.T) {
 	log := NewLog()
-	record := Record{Value: []byte("testlog")}    // recordの作成
+	record := Record{Value: []byte("abcde")}    // recordの作成
 	offset, err := log.Append(record)			// logオブジェクトに対してappend関数を実行
-
+	fmt.Printf("log: %v, record: %v, offset: %v, err: %v\n", log, record, offset, err)
 	if err != nil {
 		t.Errorf("Append returned unexpected error: %v", err)
 	}
@@ -18,7 +19,7 @@ func TestLogAppend(t *testing.T) {
 		t.Errorf("Append returned unexpected offset: got %v, want 0", offset)
 	}
 
-	if !reflect.DeepEqual(log.records[0], record) {    // !reflectとは？
+	if !reflect.DeepEqual(log.records[0], record) {    // !reflectとは？ log.records[0]とrecordの内容が等しい華道家を判定している(同じ型，変数の内容まで比較する)
 		t.Errorf("Append did not store corret record: got %v, want %v", log.records[0], record)
 	}
 }
@@ -29,7 +30,7 @@ func TestLogRead(t *testing.T) {
 	log.records = append(log.records, record)
 
 	got, err := log.Read(0)
-
+	fmt.Printf("log: %v, record: %v, got: %v, err: %v\n", log, record, got, err)
 	if err != nil {
 		t.Errorf("Read returned unexpected error: %v", err)
 	}
